18f9673f9e916cca7d98975d3d8cdc0e23952685
vafscbouchd <donald.boucher@va.gov>
2022-04-26 15:13:42 -0500

    Updating ACI scripts

---
+++ ACI/aci-generic-post-request.py
@@ -0,0 +1,39 @@
+import urllib3
+import json
+import requests
+
+username = input('username: ')
+password = input('password: ')
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+url = "https://10.219.102.76/api/aaaLogin.json"
+payloadDict = {
+    "aaaUser": {
+        "attributes": {
+            "name": username,
+            "pwd": password
+        }
+    }
+}
+payloadJson = json.dumps(payloadDict)
+header = {
+    'Content-Type': 'application/json',
+}
+token = requests.post(url, verify=False, data=payloadJson, headers=header, auth=(username, password))
+tokenJson = token.json()
+imdata = tokenJson['imdata']
+imdataDict = imdata[0]
+aaaLogin = imdataDict['aaaLogin']
+attribute = aaaLogin['attributes']
+token = attribute['token']
+# Post url and payload below
+url = 'generic url'
+payload = 'generic payload'
+
+# Execute POST
+header = {
+    'Cookie': f'APIC-cookie={token}',
+    'Authorization': 'Bearer' + ' ' + token
+}
+request = requests.post(url, headers=header, data=payload, verify=False)
+response = request.status_code
+print(response)
